53da261f307813a6128a4e26cc17bea345e42bba
Santiago Moyano <santiago98moy@gmail.com>
2022-03-22 19:19:01 -0300

    modifique app.py y borre archivo txt requirimientos

--- Backend/app.py
+++ Backend/app.py
@@ -1,19 +1,11 @@
-#Instalar python3-venv para poder manejar entornos virtuales
-#Importar Flask
-from flask import Flask
-#Importar librería os
+from main import create_app
 import os
-#Importar DotEnv para manejar variables de entorno
-from dotenv import load_dotenv
 
-#Cargar variables de entorno de archivo .env
-load_dotenv()
-#Inicializar aplicación Flask
-app = Flask(__name__)
+#Llamar a la función y devolver la app
+app = create_app()
+#Hacer push sobre el contexto de la aplicación
+#Esto permite acceder a las propiedades de la aplicación en cualquier parte del sistema
+app.app_context().push()
 
-#Verificar que el script se este ejecutando directamente
-if __name__ == '__main__':
-    #Correr servidor web
-    #Debug: Si está activado muestra mensajes de error y se reinicia al encontrar cambios
-    #Port: Puerto en el que va a correr el servicio. Obtenido de las variables de entorno
+if __name__ == '__main__':    
     app.run(debug = True, port = os.getenv("PORT"))
